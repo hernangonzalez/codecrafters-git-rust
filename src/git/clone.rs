8828e8d66d7382cb6c0ab9ee9bc3b40e1a33996f
Hernán G. Gonzalez <camus.sonico@gmail.com>
2023-06-01 11:45:52 +0100

    Clone: get pack

--- src/git/clone.rs
+++ src/git/clone.rs
@@ -1,9 +1,14 @@
+use super::Sha;
 use anyhow::{ensure, Context, Result};
 use bytes::{BufMut, Bytes, BytesMut};
-use reqwest::{Client, Url};
+use reqwest::{header, Client, Url};
 use std::{path::Path, str::FromStr};
 
-use super::Sha;
+const GIT_PACK_WANT: &[u8] = b"0032want ";
+const GIT_PACK_NEWLINE: u8 = b'\n';
+const GIT_PACK_END: &[u8] = b"0000";
+const GIT_PACK_DONE: &[u8] = b"0009done";
+const HTTP_CONTENT_TYPE: &str = "application/x-www-form-urlencoded";
 
 pub struct Repository {
     url: Url,
@@ -70,17 +75,28 @@ impl GitTransfer {
         Ok(vec)
     }
 
+    /// ref https://man.archlinux.org/man/gitprotocol-pack.5.en
     async fn pack_from(&self, refs: &[GitRef]) -> Result<Bytes> {
+        let url = format!("{}/git-upload-pack", self.remote);
+
         let mut body = BytesMut::new();
         for r in refs {
-            body.put_slice(b"0032want ");
+            body.put_slice(GIT_PACK_WANT);
             body.put_slice(r.sha.as_bytes());
-            body.put_u8(b'\n');
+            body.put_u8(GIT_PACK_NEWLINE);
         }
-        body.put_slice(b"0000");
+        body.put_slice(GIT_PACK_END);
+        body.put_slice(GIT_PACK_DONE);
+        body.put_u8(GIT_PACK_NEWLINE);
+        let body = body.freeze();
 
-        let url = format!("{}/git-upload-pack", self.remote);
-        let req = self.client.post(url).body(body.freeze()).build()?;
+        let req = self
+            .client
+            .post(url)
+            .header(header::CONTENT_TYPE, HTTP_CONTENT_TYPE)
+            .body(body)
+            .build()?;
+
         let res = self.client.execute(req).await?;
         ensure!(res.status() == 200);
         let bytes = res.bytes().await?;
